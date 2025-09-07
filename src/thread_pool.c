660c271faecb4f4e1e026c36f5396ed842269b91
Amar A.J Bouakaz <amarbouakaz91@gmail.com>
2025-09-07 20:05:34 +0100

    feat: add thread pool

---
+++ src/thread_pool.c
@@ -0,0 +1,138 @@
+
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "thread_pool.h"
+
+pthread_mutex_t mutex_queue;
+sem_t           sem_queue;
+
+pthread_t *init_thread_pool(int num_threads, task_queue *queue) {
+
+    pthread_mutex_init(&mutex_queue, NULL);
+    sem_init(&sem_queue, 0, 1);
+
+    if (queue == NULL)
+        queue = create_task_queue();
+
+    pthread_t *threads = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
+    if (!threads) {
+        perror("Failed to allocate memory for threads");
+        return NULL;
+    }
+    for (int i = 0; i < num_threads; i++) {
+        if (pthread_create(&threads[i], NULL, start_thread, queue) != 0) {
+            perror("Failed to create thread");
+            free(threads);
+            return NULL;
+        }
+    }
+    return threads;
+}
+
+int destroy_thread_pool(int num_threads, pthread_t *threads) {
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    free(threads);
+    return 0;
+}
+
+void *start_thread(void *arg) {
+    task_queue *queue = (task_queue *)arg;
+    while (1) {
+        sem_wait(&sem_queue);
+        task *t = (task *)get_task(queue);
+        sem_post(&sem_queue);
+        if (t) {
+            t->function(t->arg);
+            free(t);
+        }
+    }
+    return NULL;
+}
+
+task_queue *create_task_queue() {
+    task_queue *queue = malloc(sizeof(task_queue));
+    if (!queue) {
+        perror("Failed to create task queue");
+        return NULL;
+    }
+    queue->queue_size = 0;
+    queue->first      = NULL;
+    queue->last       = NULL;
+    return queue;
+}
+
+int destroy_task_queue(task_queue *queue) {
+    if (!queue)
+        return -1;
+
+    task_node *current = queue->first;
+    while (current) {
+        task_node *tmp = current;
+        current        = current->next;
+        free(tmp);
+    }
+
+    free(queue);
+    return 0;
+}
+
+void add_task(task_queue *queue, void (*function)(void *), void *arg) {
+
+    pthread_mutex_lock(&mutex_queue);
+    if (!queue)
+        return;
+
+    task_node *new_node = malloc(sizeof(task_node));
+    if (!new_node) {
+        perror("Failed to allocate task_node");
+        return;
+    }
+
+    new_node->task.function = function;
+    new_node->task.arg      = arg;
+    new_node->next          = NULL;
+
+    if (queue->last) {
+        queue->last->next = new_node;
+    } else {
+        queue->first = new_node;
+    }
+
+    queue->last = new_node;
+    queue->queue_size++;
+    pthread_mutex_unlock(&mutex_queue);
+}
+
+void *get_task(task_queue *queue) {
+    pthread_mutex_lock(&mutex_queue);
+    if (!queue || !queue->first) {
+        return NULL;
+    }
+
+    task_node *node = queue->first;
+    queue->first    = node->next;
+    if (!queue->first) {
+        queue->last = NULL;
+    }
+
+    void *arg                = node->task.arg;
+    void (*function)(void *) = node->task.function;
+
+    task *ret_task = malloc(sizeof(task));
+    if (ret_task) {
+        ret_task->function = function;
+        ret_task->arg      = arg;
+    }
+
+    free(node);
+    queue->queue_size--;
+    pthread_mutex_unlock(&mutex_queue);
+
+    return ret_task;
+}
